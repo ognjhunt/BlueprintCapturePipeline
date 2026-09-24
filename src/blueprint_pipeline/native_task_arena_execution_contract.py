5897afd3e5ee79c6e9c989b3217309a0a216434f
ognjhunt <ohstnhunt@gmail.com>
2026-09-24 14:51:46 -0500

    Ship G1 policy adapters in provider source closure

--- src/blueprint_pipeline/native_task_arena_execution_contract.py
+++ src/blueprint_pipeline/native_task_arena_execution_contract.py
@@ -182,6 +182,8 @@ CONTROLS_RUNTIME_MODULE_NAMES = (
 )
 
 POLICY_EXTRA_RUNTIME_MODULE_NAMES = (
+    "native_g1_humanoidarena_interface.py",
+    "native_g1_joint_episode_environment.py",
     "native_task_asset_composition_gate.py",
     "native_task_composition_diagnostic.py",
     "native_task_composition_worker.py",
